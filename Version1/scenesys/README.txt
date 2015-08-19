4e02c6962d63c25ebcdcadf9e3a81c2b3780d6b9
Andrew Bastien <volundmush@gmail.com>
2015-08-19 03:26:25 -0400

    Pre-2.0 Re-organization.
    MOVED ALL OLD CODE to the Version1 folder. Now beginning work on a much
    streamlined thing.
    
    SERIOUSLY, don't use files in this folder, they probably won't work. You
    have been warned.

---
+++ Version1/scenesys/README.txt
@@ -0,0 +1,3 @@
+With any luck this file shall soon hold information on how to setup SceneSys.
+
+It's a doozie of a project.
---
+++ Version1/scenesys/scene/db.php
@@ -0,0 +1,20 @@
+<?
+	$username="your_username";  // We highly suggest the use of a SELECT PERMISSIONS only account.
+	$password="some_password"; // This way, there is no security risk. SceneSys doesn't require more at this moment.
+	$database="your_database";
+	$site="example.com";
+	$posecount = 0;
+	
+	$db_prefix = "scene_";
+	$db_players = "players";
+	$db_scenes = "scenes";
+	$db_poses = "poses";
+	$db_schedule = "schedule";
+	
+	$scenedb = new mysqli($site,$username,$password,$database);
+	if ($scenedb->connect_errno)
+	{
+		echo "Failed to connect to MySQL: (" . $scenedb->connect_errno . ") " . $scenedb->connect_error;
+	}
+	
+?>
